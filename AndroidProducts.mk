0585c170a61bb44e851d1d89a0de76b42015552b
lynnnnzx <rifqikanekii140202@gmail.com>
2022-03-22 19:09:35 +0700

    juice: Bringup PalladiumOS
    Signed-off-by: lynnnnzx <rifqikanekii140202@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_juice.mk
+    $(LOCAL_DIR)/palladium_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_juice-user \
-    aosp_juice-userdebug \
-    aosp_juice-eng
+    palladium_juice-user \
+    palladium_juice-userdebug \
+    palladium_juice-eng
